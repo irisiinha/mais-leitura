4629519d87d68c0afcbdd43d9a30f23dcba2229a
irisiinha <irismeiresilvaoliveira@gmail.com>
2025-07-31 17:27:10 +0000

    ok

--- models/promisse.js
+++ models/promisse.js
@@ -4,6 +4,21 @@ const terminal = readFile.createInterface({
   output: process.stdin,
 });
 
-terminal.question("qual seu nome: \n",function (valor){
-  console.log("seja bem vindo" + valor);
+// terminal.question("qual seu nome: \n",ction (valor){
+//   console.log("seja bem vindo" + valor);function (valor){
+//   console.log("seja bem vindo" + valor);
+// });
+
+function question(pergunta) {
+  return new Promise(function (resolve, reject){
+    terminal.question(pergunta + "\n", function(valor){
+      resolve(valor)
+    });
+  });
+}
+question("qual seu nome").then(function(valor){
+  console.log(`seja bem vindo ${valor}`); 
+})
+.finally(function (){
+  terminal.close();
 });
