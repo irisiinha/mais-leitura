cb7dd68bfb2f6dda5300977e405e963aba66aeed
irisiinha <irismeiresilvaoliveira@gmail.com>
2025-09-25 14:30:41 +0000

    fim

--- infra/database.js
+++ infra/database.js
@@ -1,11 +1,18 @@
-const {Client} = require("pg");
-const { prototype } = require("pg/lib/type-overrides");
+const { Client } = require("pg");
 
-function query (sql){
-const client = new Client({
-    host: "",
+async function query(sql, args = []) {
+  const client = new Client({
+    host: "localhost",
     port: 5432,
-    user:"" ,
-    password: "",
-  })
+    user: "iris",
+    database: "maisleitura",
+    password: "1234",
+  });
+  await client.connect();
+
+  const res = await client.query(sql, args);
+
+  await client.end();
+  return res.rows;
 }
+module.exports = { query };
