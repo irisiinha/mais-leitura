203ce1a96ba4ab31bc8c784be40dfd15e3e5090e
irisiinha <irismeiresilvaoliveira@gmail.com>
2025-10-01 18:30:59 +0000

    fim

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function hello() {
-  console.log("sad")
+const {query} = require("./infra/database");
 
-}
-const pessoa_nome-
+console.log(process.env.NAME);
+
+// query("SELECT 1 + 1;")
+//  .then(function(value) {
+//    console.log(value);
+//  })
+//  .catch(function(err) {
+//    console.log ("Deu ruim", err);
+ });
