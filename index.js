f0b63edf32279947e6af68e2196cf371467dc3c3
irisiinha <irismeiresilvaoliveira@gmail.com>
2025-11-05 17:18:09 +0000

    fim

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const livroRepositorio = require('./infra/repoository/livro');
+const http = require("node:http");
 
-livroRepositorio
-.consutarTodos()
-  .then(function (resultados) {
-  console.log(resultados);
-})
-.catch(function (erro) {
-console.log("Deu erro: ", erro);
+const server = http.createServer(function (req, res){
+  //res.writHead(200, {'Content-Type': 'text/plain'});
+  console.log(req.method, req.url);
+  res.end("ok bem vindo")
+});
+
+const port = process.env.PORT || 3001;
+server.listen(port, function(){
+  console.log("incializando sevidor http na porta" + port);
 });
